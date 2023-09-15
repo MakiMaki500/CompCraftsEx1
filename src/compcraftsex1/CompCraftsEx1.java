bc375de206c819b9a0e6d735d2fae40bab439430
josue <josue@172.25.10.131>
2023-09-15 10:59:52 +0800

    template

--- src/compcraftsex1/CompCraftsEx1.java
+++ src/compcraftsex1/CompCraftsEx1.java
@@ -1,12 +1,15 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package compcraftsex1;
 
+import java.util.Scanner;
+
 /**
  *
- * @author josue
+ * @author admin
  */
 public class CompCraftsEx1 {
 
@@ -15,6 +18,70 @@ public class CompCraftsEx1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        String repeat = "y";
+        
+        while(repeat.equalsIgnoreCase("y")){
+        System.out.println("What do you want to display? ");
+        System.out.println("A. Right Triangle");
+        System.out.println("B. Inverted Right Triangle");
+        System.out.println("C. Rectangle with alternating rows");
+        System.out.println("D. Rectangle with borders");
+        System.out.println("E. Rectangle with alternating rows and columns");
+        System.out.println("Enter the letter of your choice: ");
+        
+        String choice = sc.nextLine();
+        
+        //Right triangle 
+        if(choice.equalsIgnoreCase("a")){
+        
+            //put answer here
+        }
+        
+        //inverted right triangle
+        else if(choice.equalsIgnoreCase("b")){
+            
+            //put answer here
+        
+        }
+        
+        
+        //rectangle with alternating shapes per row
+        else if(choice.equalsIgnoreCase("c")){
+        
+            //put answer here
+            
+        }
+        
+        
+        //rectangle with border
+        else if(choice.equalsIgnoreCase("d")){
+        
+            //put answer here
+            
+        }
+        
+        
+        //rectangle with alternating shapes per row
+        else if(choice.equalsIgnoreCase("e")){ 
+        
+            //put answer here
+            
+        }
+        
+        else{
+            System.out.println("Choose a letter from A-E");
+        }
+        
+        System.out.println("Again? [Y/N]: ");
+        repeat = sc.nextLine();
+    }
+        
+        
+        
+        
+        
+        
     }
     
 }
